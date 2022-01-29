b658d6073ce5fa799c7a556dccd3761cb051368e
phongsun <phsun@smu.edu>
2022-01-29 17:02:08 -0600

    fixed bugs that made it not work

--- routes.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { ProductList } from './app/productList';
-import { ProductDetails } from './app/productDetails';
-
-export const ROUTES = [
-    { path: '/', exact: true, element: <ProductList cartService={cartService}/> },
-    { path: '/products/:productId', element: <ProductDetails cartService={cartService}/> },
-    { path: '/cart', element: <MyCart/> }
-];
---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ProductList } from './app/productList';
+import { ProductDetails } from './app/productDetails';
+import CartService from './services/cartService';
+import { MyCart } from './app/myCart';
+
+export const ROUTES = [
+    { path: '/', exact: true, element: <ProductList cartService={CartService}/> },
+    { path: '/products/:productId', element: <ProductDetails cartService={CartService}/> },
+    { path: '/cart', element: <MyCart/> }
+];
